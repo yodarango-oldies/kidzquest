7ef64035a5664d63dcdcb1c6947003e6cf1e702c
Daniel Rangel <61793148+yodarango@users.noreply.github.com>
2022-06-15 11:50:35 -0400

    update to styles to reflect broader generalization

--- frontend/src/components/pages/SingleChild.js
+++ frontend/src/components/pages/SingleChild.js
@@ -1,116 +1,255 @@
-import { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
-import myTime from '../../helpers/myTime'
-import Cookie from 'js-cookie'
-
-const SingleChild = () => {
-  const [toggleClass, settoggleClass] = useState('')
-  const [temporaryTime, settemporaryTime] = useState('')
-  const [childFetched, setchildFetch] = useState([])
-
-  const params = useLocation()
-  const childId = params.search.split('=')[1]
-  console.log(childId)
-
-  const fecthChild = async () => {
-    const req = await fetch(`/getchild/${childId}`)
-    const child = await req.json()
-    setchildFetch(child)
-    console.log(child)
-
-    // fetch the toggle according to the checked in status
-    child.checkedInOptions.checkedIn === false
-      ? settoggleClass('checkedStatus_out')
-      : child.checkedInOptions.checkedIn === true
-      ? settoggleClass('checkedStatus_in')
-      : null
-  }
-
-  useEffect(() => {
-    fecthChild()
-  }, [])
-
-  // ====================  handle checkin ====================== //
-  const handleCheckIn = async (id) => {
-    settoggleClass('checkedStatus_in')
-    settemporaryTime(myTime())
-    const req = await fetch(`/checkin/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ status: true }),
-    })
-    console.log(req)
-  }
-
-  const handleCheckOut = async (id) => {
-    settemporaryTime(myTime())
-    settoggleClass('checkedStatus_out')
-    const req = await fetch(`/checkout/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ status: false }),
-    })
-  }
-  // ================= check that the user is logged in ===========
-  const [login] = useState(Cookie.get('logedin'))
-  return (
-    <div className="child-main-wrapper">
-      <div className="child_photo_wrapper">
-        <div className="child_photo" style={{ backgroundImage: `url(${childFetched.photo})` }}></div>
-      </div>
-      <h1 className="std-title">First Name:</h1>
-      <h2 className="std-title-white">{childFetched.childFirstName}</h2>
-      <h1 className="std-title">Last Name:</h1>
-      <h2 className="std-title-white">{childFetched.childLastName}</h2>
-      <h2 className="std-title">Age:</h2>
-      <h2 className="std-title-white">{childFetched.childAge}</h2>
-      <h2 className="std-title">Guardian's Name:</h2>
-      <h2 className="std-title-white">{childFetched.guardianName}</h2>
-      <h2 className="std-title">Guardian's Phone #:</h2>
-      <h2 className="std-title-white">{childFetched.guardianPhoneNumber}</h2>
-
-      {login && (
-        <section className="child-progile-row-wrapper">
-          {toggleClass === 'checkedStatus_in' && (
-            <div className={`child-profile-row_checkedStatusIn ${toggleClass}`}>
-              <span className={`checkedStatus_ball`} onClick={() => handleCheckOut(childFetched._id)}></span>
-            </div>
-          )}
-          {toggleClass === 'checkedStatus_out' && (
-            <div className={`child-profile-row_checkedStatusOut ${toggleClass}`}>
-              <span className={`checkedStatus_ball`} onClick={() => handleCheckIn(childFetched._id)}></span>
-            </div>
-          )}
-          {toggleClass === 'checkedStatus_in' && (
-            <p className="child-profile-row_status">
-              Checked-in at: {childFetched.checkedInOptions.checkedInTime} {temporaryTime}
-            </p>
-          )}
-          {toggleClass === 'checkedStatus_out' && (
-            <p className="child-profile-row_status">
-              Checked-out at: {childFetched.checkedInOptions.checkedOutTime}
-              {temporaryTime}
-            </p>
-          )}
-        </section>
-      )}
-
-      {toggleClass === 'checkedStatus_in' && (
-        <div className="child-row_status_in">
-          <p>Currently checked-in</p>
-        </div>
-      )}
-      {toggleClass === 'checkedStatus_out' && (
-        <div className="child-row_status_out">
-          <p>Has been checked out since {childFetched.checkedInOptions.checkedOutTime} </p>
-        </div>
-      )}
-    </div>
-  )
+*{
+	font-size: 100%;
+	border: none;
+	outline: 0;
+	padding: 0;
+    margin: 0;
+    list-style: none;
+    font-style: none;
+    box-sizing: border-box;
 }
 
-export default SingleChild
+html{
+    font-size: 10px;
+}
+
+body{
+    overflow-x: hidden;
+    line-height: 1.8em;
+}
+
+a:visited, a:active, a:link
+{
+    color: inherit;
+    text-decoration: none;
+}
+
+*::-webkit-scrollbar {
+    display: none;
+  }
+  /* Hide scrollbar for IE, Edge and Firefox */
+*{
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
+
+/* ------------------------ GENERAL STYLES --------------------------  */
+@import url("font-url");
+@import url("font-url");
+@import url("font-url");
+
+:root {
+   /*-------------- Fonts----------*/
+   --body-font: 400 1.2rem acumin-pro, sans-serif;
+   --title-font: 700 1.563rem acumin-pro, sans-serif;
+   --secondary-font: 300 italic 1.16rem acumin-pro, sans-serif;
+   --accent-font: 700 2.83rem acumin-pro, sans-serif;
+
+
+   /*------------- Colors ---------------*/
+   --primary-color: #2a2438;
+   --secondary-color: #433b55;
+   --third-color: #4f3b78;
+   --fourth-color: #5c5470;
+   --accent-color: #5c5470;
+   --font-color: #f2f2f2;
+   --warning-color: #ec3b3b;
+   --success-color: #90d171;
+
+   /*--------- Spacing Specs --------*/
+   --xlarge-spacing: 5rem;
+   --large-spacing: 3rem;
+   --medium-spacing: 1.8rem;
+   --small-spacing: 1.5rem;
+   --xsmall-spacing: 0.8rem;
+
+    /*-------- Sizing Specs --------*/
+   --icon-sizing: 2rem;
+   --button-padding: 0.5rem 0;
+   --input-padding: 0.5rem;
+
+   /*---------- Main Wrapper --------*/
+   --main-wrapper: 92%;
+   --max-width-main-wrapper: 50rem;
+
+
+   /*------------ Breaks ------------*/
+   --ipad-break: 600px;
+   --laptop-break: 800px;
+   --desktop-break: 1050px;
+}
+
+
+@media (min-width: var(--ipad-break)) {
+    
+    :root{
+        /*------------ Spacing Specs -----------*/
+        --xlarge-spacing: 5.4rem;
+        --large-spacing: 3.3rem;
+        --medium-spacing: 2rem;
+        --small-spacing: 1.7rem;
+        --xsmall-spacing: 0.9rem;
+
+        /*------------- Main Wrapper -------------*/
+        --max-width-main-wrapper: 80rem;
+    }
+    
+}
+
+@media (min-width: var(--desktop-break)) {
+    
+    :root{
+          /*----------- Fonts -------------*/
+          --body-font: 400 1.35rem acumin-pro, sans-serif;
+          --title-font: 700 1.875rem acumin-pro, sans-serif;
+          --secondary-font: 300 italic 1.3rem acumin-pro, sans-serif;
+          --accent-font: 700 1.35rem acumin-pro, sans-serif;
+       
+        /*----------- Spacing Specs ----------*/
+        --xlarge-spacing: 5.6rem;
+        --large-spacing: 3.5rem;
+        --medium-spacing: 2.2rem;
+        --small-spacing: 1.9rem;
+        --xsmall-spacing: 1rem;
+
+        /*------------ Sizing Specs -------------*/
+        --icon-sizing: 2.5rem;
+        --button-padding: 0.6rem 0;
+        --input-padding: 0.6rem;
+
+       /*------------- Main Wrapper -------------*/
+        --max-width-main-wrapper: 62.5rem;
+
+    }
+}
+
+/*------------------------GENERAL STYLING -----------------------*/
+h1, h2, h3, h4, h5, h6, .std-title{
+    font: var(--title-font);
+    color: var(--font-color);
+    text-align: center;
+    margin: var(--medium-spacing) auto;
+    width: 100%;
+}
+
+h2{
+    font-size: 1.688rem;
+}
+
+h3{
+    font-size: 1.563rem;
+    margin: var(--small-spacing) auto;
+}
+
+h4, .std-title--small{
+    font-size: 1.375rem;
+    margin: var(--small-spacing) auto;
+}
+
+h5, h6{
+    font-size: 1.2rem;
+    margin: var(--small-spacing) auto;
+}
+
+p, .std-text, .std-text--secondary {
+    font: var(--body-font);
+    color: var(--font-color);
+    text-align: center;
+    margin: var(--medium-spacing) auto;
+    width: 100%;
+}
+
+/*-------------------------------- STANDARDS ------------------------------------*/
+/*--------------- Buttons:                                                      */
+button, .std-button{
+    display: block;
+    padding: var(--button-padding);
+    text-align: center;
+    border: none;
+}
+
+button:hover, .std-button:hover{
+    cursor: pointer;
+}
+
+/*--------------- Input:                                                       */
+input[type=text], input[type=email], input[type=tel], input[type=url], .std-input{
+    display: block;
+    padding: var(--input-padding);
+    font: var(--primary-font);
+    color: var(--font-color);
+    outline: none;
+    border: none;
+}
+
+input[type=radio], input[type=file], input[type=checkbox] {
+    display: none;
+}
+
+/*--------------- Text:                                                        */
+.std-text--secondary{
+    font: var(--secondary-font);
+}
+
+/*--------------- Images:                                                      */
+img, .std-img {
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+    -o-object-fit: cover;
+    -o-object-position: center;
+    display: block;
+    margin: var(--medium-spacing) auto;
+}
+
+.std-background
+{
+    display: block;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+
+.std-icon{
+    display: block;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: var(--icon-sizing);
+    height: var(--icon-sizing);
+}
+
+/*--------------- Containers:                                                     */
+.std-flex-row, .std-flex-column, .std-flex--nowrap,
+ .std-flex--start, .std-flex--end {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+ }
+ 
+ .std-flex-column {
+    flex-flow: column;
+ }
+
+ .std-flex-nowrap{
+     flex-wrap: nowrap;
+ }
+
+ .std-flex-justify-start{
+    justify-content: flex-start;
+ }
+
+ .std-flex-justify-end{
+    justify-content: flex-end;
+ }
+
+ .std-flex-align-start{
+    justify-content: flex-start;
+ }
+
+ .std-flex-align-end{
+    justify-content: flex-end;
+ }
